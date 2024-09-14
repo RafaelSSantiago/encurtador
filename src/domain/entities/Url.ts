07a34d8b756c115b7539a52e0df9471ae1eb9649
RafaelSSantiago <raphasantiago768@gmail.com>
2024-09-14 00:30:07 -0300

    feat: add entidades

---
+++ src/domain/entities/Url.ts
@@ -0,0 +1,9 @@
+export class Url {
+  constructor(
+    public id: number,
+    public originalUrl: string,
+    public shortenedUrl: string,
+    public clicks: number,
+    public userId?: number
+  ) {}
+}
