de9329ae1247513481ea0fccf8fe107b398687e5
RafaelSSantiago <raphasantiago768@gmail.com>
2024-09-15 14:03:40 -0300

    refactor: controller

--- src/interfaces/controllers/UserController.ts
+++ src/interfaces/controllers/UserController.ts
@@ -1,28 +1,22 @@
 import { RegisterUserUseCase } from "../../application/RegisterUserUseCase";
 import { Request, Response } from "express";
-import { UserExistingValidation } from "../../domain/validators/user-existing-validation";
-import { conflict, ok, serverError } from "../../domain/helpers/httpHelpers";
-import { ExistingUser } from "../../domain/errors/ExistingUser.error";
+import { badRequest, ok, serverError } from "../../domain/helpers/httpHelpers";
+import { RequiredFieldValidation } from "../../domain/validators/RequiredFieldValidation";
 
 export class Usercontroller {
   constructor(private registerUserUseCase: RegisterUserUseCase) {}
 
   async createUSerdDb(req: Request, res: Response) {
     try {
-      const userExisting = await new UserExistingValidation(
-        req.body.email
-      ).validate();
-
-      if (userExisting) {
-        const conflictResponse = conflict(new ExistingUser());
-        return res
-          .status(conflictResponse.statusCode)
-          .json(conflictResponse.body);
+      for (const field of ["email", "password"]) {
+        const error = new RequiredFieldValidation(field).validate(req.body);
+        if (error) {
+          const missingParam = badRequest(error);
+          return res.status(missingParam.statusCode).json(missingParam.body);
+        }
       }
-
-      const user = await this.registerUserUseCase.execute(req.body);
-      const sucess = ok(user);
-      return res.status(sucess.statusCode).json(user);
+      const httpResponse = await this.registerUserUseCase.execute(req.body);
+      return res.status(httpResponse.statusCode).json(httpResponse.body);
     } catch (error) {
       const errorResponse = serverError(error as any);
       return res.status(errorResponse.statusCode).json(errorResponse.body);
