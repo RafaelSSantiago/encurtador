b8352df18f7a8bcf22ff4512baf0a8b794e1147c
RafaelSSantiago <raphasantiago768@gmail.com>
2024-09-15 18:13:15 -0300

    refactor: params

--- src/domain/entities/Url.ts
+++ src/domain/entities/Url.ts
@@ -2,8 +2,9 @@ export class Url {
   constructor(
     public id: number,
     public originalUrl: string,
-    public shortenedUrl: string,
-    public clicks: number,
-    public userId?: number | null
+    public shortenedUrl?: string,
+    public clicks?: number,
+    public userId?: number | null,
+    public jwtUser?: any
   ) {}
 }
