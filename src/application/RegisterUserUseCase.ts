63f4628c2fe8ec7be792aa3ca7512dc4385f6a35
RafaelSSantiago <raphasantiago768@gmail.com>
2024-09-15 14:03:58 -0300

    refactor: caso de uso

--- src/application/RegisterUserUseCase.ts
+++ src/application/RegisterUserUseCase.ts
@@ -1,10 +1,19 @@
 import { User } from "../domain/entities/User";
+import { ExistingUser } from "../domain/errors/ExistingUser.error";
+import { conflict, ok } from "../domain/helpers/httpHelpers";
+import { UserExistingValidation } from "../domain/validators/user-existing-validation";
 import { UserRepository } from "../infrastructure/repositories/UserRepository";
+import { HttpResponse } from "../domain/protocols/http";
 
 export class RegisterUserUseCase {
   constructor(private userRepository: UserRepository) {}
 
-  async execute(user: User) {
-    return await this.userRepository.create(user);
+  async execute(user: User): Promise<HttpResponse> {
+    const userExisting = new UserExistingValidation(user.email);
+    if (await userExisting.validate()) {
+      return conflict(new ExistingUser());
+    }
+    const newUser = await this.userRepository.create(user);
+    return ok(newUser);
   }
 }
