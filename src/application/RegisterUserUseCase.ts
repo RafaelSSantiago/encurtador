6616f1bd6c327d6e041fc592fc391281ae558dfd
RafaelSSantiago <raphasantiago768@gmail.com>
2024-09-15 18:18:54 -0300

    refactor: resolvendo import

--- src/application/RegisterUserUseCase.ts
+++ src/application/RegisterUserUseCase.ts
@@ -1,7 +1,7 @@
 import { User } from "../domain/entities/User";
 import { ExistingUser } from "../domain/errors/ExistingUser.error";
 import { conflict, ok } from "../domain/helpers/httpHelpers";
-import { UserExistingValidation } from "../domain/validators/user-existing-validation";
+import { UserExistingValidation } from "../domain/validators/User-existing-validation";
 import { UserRepository } from "../infrastructure/repositories/UserRepository";
 import { HttpResponse } from "../domain/protocols/http";
 
