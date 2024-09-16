f02d7ad90fc50929735b4ff9e9b436423a931507
RafaelSSantiago <raphasantiago768@gmail.com>
2024-09-16 11:42:05 -0300

    feat: add dto

---
+++ src/dtos/responseUrl.dto.ts
@@ -0,0 +1,11 @@
+interface UrlDTO {
+  id: number;
+  originalUrl: string;
+  shortenedUrl: string;
+  clicks: number;
+  userId?: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt?: Date | null;
+}
+
