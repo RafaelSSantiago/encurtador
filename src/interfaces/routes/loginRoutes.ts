2a3a6b2925b0ba898757e828fdacf327c0eb02da
RafaelSSantiago <raphasantiago768@gmail.com>
2024-09-16 20:45:41 -0300

    doc: documentado roteadores

--- src/interfaces/routes/loginRoutes.ts
+++ src/interfaces/routes/loginRoutes.ts
@@ -3,12 +3,20 @@ import { LoginController } from "../controllers/LoginController";
 import LoginUserUseCase from "../../application/LoginUserCase";
 import LoginUserRepository from "../../infrastructure/repositories/LoginUserRepository";
 
+/**
+ * Roteador para as rotas de login.
+ */
 const router = Router();
 
 const loginRepository = new LoginUserRepository();
 const loginUseCase = new LoginUserUseCase(loginRepository);
 const loginController = new LoginController(loginUseCase);
 
+/**
+ * @route POST /login
+ * @description Realiza o login de um usuário
+ * @access Public
+ */
 router.post("/", (req, res) => loginController.login(req, res));
 
 export { router as loginRoutes };
--- src/interfaces/routes/userRoutes.ts
+++ src/interfaces/routes/userRoutes.ts
@@ -4,13 +4,27 @@ import { UserRepository } from "../../infrastructure/repositories/UserRepository
 import { RegisterUserUseCase } from "../../application/RegisterUserUseCase";
 import { authMiddleware } from "../../infrastructure/middlewares/AuthMiddleware";
 
+/**
+ * Roteador para as rotas de usuários.
+ */
 const router = Router();
 
 const userRepository = new UserRepository();
 const registerUserUseCase = new RegisterUserUseCase(userRepository);
 const userController = new Usercontroller(registerUserUseCase);
 
+/**
+ * @route POST /user
+ * @description Registra um novo usuário
+ * @access Public
+ */
 router.post("/", (req, res) => userController.createUSerdDb(req, res));
+
+/**
+ * @route GET /user/url
+ * @description Lista todas as URLs encurtadas do usuário autenticado
+ * @access Private (Requer autenticação)
+ */
 router.get("/url", authMiddleware, (req, res) =>
   userController.listUserUrls(req, res)
 );
