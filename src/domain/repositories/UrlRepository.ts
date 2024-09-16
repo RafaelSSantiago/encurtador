52bc37e00094d754c6b37c598eb7c8c5f54ef6c5
RafaelSSantiago <raphasantiago768@gmail.com>
2024-09-16 11:42:19 -0300

    refactor: findByUserId

--- src/domain/repositories/UrlRepository.ts
+++ src/domain/repositories/UrlRepository.ts
@@ -4,7 +4,7 @@ import { Url } from "../entities/Url";
 export interface UrlRepository {
   create(url: Url): Promise<Url>;
   findByShortenedUrl(shortenedUrl: string): Promise<string | undefined>;
-  findByUserId(userId: number): Promise<Url[]>;
+  findByUserId(userId: string): Promise<UrlDTO[]>;
   updateClicks(url: Url): Promise<void>;
   updateUrl(url: Url): Promise<void>;
   softDelete(body: DeleteUrlDTO): Promise<void>;
