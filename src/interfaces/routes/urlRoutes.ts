2108e8268b919414b63c31a35b5f58ee880081ee
RafaelSSantiago <raphasantiago768@gmail.com>
2024-09-16 20:34:43 -0300

    doc: url route

--- src/interfaces/routes/urlRoutes.ts
+++ src/interfaces/routes/urlRoutes.ts
@@ -7,6 +7,9 @@ import { RedirectUrlUseCase } from "../../application/RedirectUrlUseCase";
 import { DeleteUrlUseCase } from "../../application/DeleteUrlUseCase";
 import { UpdateUrlUseCase } from "../../application/UpdateUrlUseCase";
 
+/**
+ * Roteador para as rotas de URL.
+ */
 const router = Router();
 
 const urlRepository = new PrismaUrlRepository();
@@ -21,13 +24,36 @@ const urlController = new UrlController(
   updateUrlUseCase
 );
 
+/**
+ * @route POST /url/shorten
+ * @description Cria uma URL encurtada
+ * @access Private (Requer ou não autenticação)
+ */
 router.post("/shorten", authMiddleware, (req, res) =>
   urlController.shortenUrl(req, res)
 );
+
+/**
+ * @route GET /url/:hash
+ * @description Redireciona para a URL original com base no hash fornecido
+ * @access Public
+ */
 router.get("/:hash", (req, res) => urlController.findHashBd(req, res));
+
+/**
+ * @route DELETE /url/:hash
+ * @description Deleta uma URL encurtada com base no hash fornecido
+ * @access Private (Requer autenticação)
+ */
 router.delete("/:hash", authMiddleware, (req, res) =>
   urlController.deleteShortenedUrl(req, res)
 );
+
+/**
+ * @route PUT /url/:hash
+ * @description Atualiza a URL original de uma URL encurtada com base no hash fornecido
+ * @access Private (Requer autenticação)
+ */
 router.put("/:hash", authMiddleware, (req, res) =>
   urlController.updateOriginalUrl(req, res)
 );
